8b3b085c235db2d0509e10cd52a2794775f1b7b2
EleonoraCannavacciuolo <eleonora.cannavacciuolo.93@gmail.com>
2025-06-26 17:44:42 +0200

    Update synoptic.js

--- docs/js/synoptic.js
+++ docs/js/synoptic.js
@@ -25,7 +25,7 @@ async function buildSynopticEdition() {
     const makeLine = (line, id) => {
       const wrapper = document.createElement('div');
       if (!line) {
-        wrapper.innerHTML = `<p class="line gap" data-align="${id}"><span class="ln"></span>
+        wrapper.innerHTML = `<p data-align="${id}"><span class="ln"></span>
         <span class="line-text">---</span></p>`;
       } else {
         line.setAttribute("data-align", id);
